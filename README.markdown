a9dfa65cd4131edd82dc038e2fda400405346ba3
Dave Lim <dlimiter@gmail.com>
2012-05-08 00:22:11 +1000

    Implemented slider and lock function

---
+++ README.markdown
@@ -0,0 +1,17 @@
+# About
+ 
+This is a Jekyll][1] powered website produced for the major Identity Brief for the Contextual Studies in the Grad Dip on Graphic Design at RMIT, Melbourne 
+
+# License
+
+The following directories and their contents are Copyright to Dave Lim. You may not reuse or republish anything therein without my permission:
+
+* `_posts/` 
+* `_drafts/`
+* `img/`
+
+All other HTML, CSS and javascript is free for your use under a [Creative Commons Attribution 3.0 Unported][3] (CC BY 3.0) license. Basically feel free to reuse it for any personal or commercial purposes, just tip your hat in my general direction. 
+
+[1]:http://github.com/mojombo/jekyll
+[2]:https://gist.github.com/639920/
+[3]:http://creativecommons.org/licenses/by/3.0/
--- js/site.js
+++ js/site.js
@@ -1,8 +1,75 @@
 ---
 ---
+var classMap =  ['one', 'two', 'three'];
+var SELECTED = "selected";
+
+var lockMode = false;
+
+var indexTop = indexMiddle = indexBottom = previousIndex = 0;
+var panelCount = 3;
 
 $(document).ready(function () {
+	$('ul#controls .lock').click(function(){
+		//Toggle selected class
+		var $this = $(this);
+		if ($this.hasClass(SELECTED)) {
+			$this.removeClass(SELECTED);
+			lockMode = false;
+		} else {
+			$this.addClass(SELECTED);
+			lockMode = true;
+		}
+		//Toggle lock mode - lock other indexes to top index
+		previousIndex = indexMiddle;
+		indexMiddle = (indexTop);
+		thisList = 'middle-flip';
+		showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexMiddle]);			
+		previousIndex = indexBottom;
+		indexBottom = (indexTop);
+		thisList = 'bottom-flip';
+		showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexBottom]);
 
-
+	});
+	
+	$('.slice').click(function() {
+		//hide this slice and show next
+		var $this = $(this);
+		var thisList = $this.closest('ul').attr('id');
+		if (lockMode) {
+			thisList = 'top-flip';			
+			indexTop = nextIndex(indexTop);
+			showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexTop]);
+			indexMiddle = indexTop;
+			thisList = 'middle-flip';
+			showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexMiddle]);			
+			indexBottom = indexTop;
+			thisList = 'bottom-flip';			
+			showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexBottom]);
+		} else {
+			if ( thisList === 'top-flip' ) {
+				indexTop = nextIndex(indexTop);
+				showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexTop]);
+			} else if ( thisList === 'middle-flip' ) {
+				indexMiddle = nextIndex(indexMiddle);
+				showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexMiddle]);
+			} else {
+				indexBottom = nextIndex(indexBottom);
+				showNextSlice('#'+thisList+' .'+classMap[previousIndex], '#'+thisList+' .'+classMap[indexBottom]);
+			}
+		}
+	});
 });
 
+function nextIndex(old) {
+	previousIndex = old;	
+	return (old+1) % panelCount;
+}
+
+function showNextSlice(oldEl, newEl) {
+	console.log(oldEl + '|'+newEl);
+	if (oldEl === newEl) {
+		return 0;
+	}
+	$(oldEl).fadeOut();
+	$(newEl).fadeIn();	
+}
